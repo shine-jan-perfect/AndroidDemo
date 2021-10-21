3c022db18a742215d9ec4ac946f862dc0119eb49
Janeyer <shine.jan.perfect@gmail.com>
2021-10-22 00:20:07 +0800

    change MusicProgressFormatUtils

--- MusicPlayerDemo/app/src/main/java/com/zuliz/musicplayerdemo/utils/MusicProgressFormatUtils.kt
+++ MusicPlayerDemo/app/src/main/java/com/zuliz/musicplayerdemo/utils/MusicProgressFormatUtils.kt
@@ -1,23 +1,36 @@
 package com.zuliz.musicplayerdemo.utils
 
-import java.text.SimpleDateFormat
-import java.util.*
-
 object MusicProgressFormatUtils {
+    /**
+     * @return mm:ss
+     */
     private fun formatMillisecond2Hms(millisecond: Long?): String {
         if (millisecond == null) {
             return "00:00"
         }
-        //这里想要只保留分秒可以写成"mm:ss"
-        val formatter = SimpleDateFormat("hh:mm:ss", Locale.getDefault())
-        //这里很重要，如果不设置时区的话，输出结果就会是几点钟，而不是毫秒值对应的时分秒数量了。
-        formatter.timeZone = TimeZone.getTimeZone("GMT+00:00")
-        return formatter.format(millisecond)
+
+        val minutes = millisecond / (1000 * 60)
+        val seconds = millisecond / (1000)
+
+        val minutesHmsText = if (minutes < 10) {
+            "0$minutes"
+        } else {
+            minutes
+        }
+        val secondsHmsText = if (seconds < 10) {
+            "0$seconds"
+        } else {
+            seconds
+        }
+        return "$minutesHmsText:$secondsHmsText"
     }
 
+    /**
+     * @return mm:ss/mm:ss
+     */
     fun formatMaxAndProgress2ProgressText(max: Int?, progress: Int?): String {
-        val maxText = formatMillisecond2Hms(max?.toLong())
-        val progressText = formatMillisecond2Hms(progress?.toLong())
-        return "$progressText/$maxText"
+        val maxHmsText = formatMillisecond2Hms(max?.toLong())
+        val progressHmsText = formatMillisecond2Hms(progress?.toLong())
+        return "$progressHmsText/$maxHmsText"
     }
 }
